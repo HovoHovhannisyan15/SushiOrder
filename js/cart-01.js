798165b261e0d00d2f3b81506301e5d7dd78f5b1
ToDo <it.engineer.95@mail.ru>
2026-06-16 16:27:37 +0300

    Собираем данные с этого товара и записываем их в единый объект productInfo

--- js/cart-01.js
+++ js/cart-01.js
@@ -6,6 +6,20 @@ window.addEventListener('click', function (event) {
     if (event.target.hasAttribute('data-cart')) {
 
         // находим карточку с товаром внутри которой был совершен клик 
+        const card = event.target.closest('.card');
 
-    }
-})
+        // Собираем данные с этого товара и записываем их в единый объект productInfo
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title: card.querySelector('.item-title').innerText,
+            itemsInbox: card.querySelector('[data-items-in-box]').innerText,
+            weight: card.querySelector('.price__weight').innerText,
+            price: card.querySelector('.price__currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+        };
+
+        console.log(productInfo);
+
+    };
+});
